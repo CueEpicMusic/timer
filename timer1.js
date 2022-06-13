04d7ccd5dbab8aeddc97fec5b3b59c90c217a220
Tom <chiatunglo@hotmail.com>
2022-06-13 19:00:03 +0000

    refactor code based on mentor's suggestion

--- timer1.js
+++ timer1.js
@@ -1,11 +1,18 @@
 let input = process.argv.slice(2);
 
-for (const i of input) {
-  if (!Number(i) || i < 0) {
-    console.log("Please provide a valid number");
-  } else {
-    setTimeout(() => {
-      process.stdout.write(`\x07`);
-    }, i * 1000);
+const timer = function() {
+  if (!input.length) {
+    console.log(null)
+  }
+  for (const i of input) {
+    if (!Number(i) || i < 0) {
+      console.log("Please provide a valid number");
+    } else {
+      setTimeout(() => {
+        process.stdout.write(`\x07`);
+      }, i * 1000);
+    }
   }
 }
+
+timer()
